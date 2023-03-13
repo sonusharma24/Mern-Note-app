37379faff572247e599661b9444d7eb46f4613c6
sonu sharma <sonusharma224447@gmail.com>
2023-03-13 21:14:54 +0530

    feat:add routes

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,11 +1,28 @@
-import express from "express";
+import express, {Request, Response,NextFunction} from "express";
 import "dotenv/config"
+import notesRoutes from "./routes/notes"
+import morgan from "morgan"
 const app = express()
 
 
-app.get("/",(req,res)=>{
-    res.send("hi, sonu how are you")
-    })
+app.use(morgan("dev"))
+app.use(express.json())
+
+app.use("/api/notes", notesRoutes)
+
+app.use((res, req, next)=>{
+    next(Error("page not found"))
+
+})
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use(( error:unknown,req :Request, res:Response,next:NextFunction  )=>{
+    console.log(error)
+    let errorMessage="an unknown error occurred"
+    if(error instanceof Error) errorMessage= error.message
+    res.status(500).json({error:errorMessage})
+
+})
     
 
 export default app
