13878218d7068ee45d981aac46651ac5179801c6
sonu sharma <sonusharma224447@gmail.com>
2023-03-14 23:04:26 +0530

    add all api methods

--- Backend/src/controllers/notes.ts
+++ Backend/src/controllers/notes.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express"
 import createHttpError from "http-errors"
 import mongoose from "mongoose"
+import note from "../models/note"
 import NoteModel from "../models/note"
 
 
@@ -60,11 +61,58 @@ try {
 }
 }
 
-export  const updateNote :RequestHandler = async(req, res, next)=>{
+interface updateNoteParams {
+    noteId:string
+}
+
+interface updateNoteBody{
+    title?:string,
+    text?:string
+}
+
+export  const updateNote :RequestHandler<updateNoteParams, unknown, updateNoteBody, unknown> = async(req, res, next)=>{
+    const noteId = req.params.noteId
+    const newTitle = req.body.title
+    const newText = req.body.text
     try {
-        
+        if(!mongoose.isValidObjectId(noteId)){
+            throw createHttpError(400, "invalid note id")
+        }
+        if(!newTitle){
+            throw createHttpError(400, "note must have a title")
+        }
+
+        const note=await NoteModel.findById(noteId).exec()
+
+        if(!note){
+            throw createHttpError(404, "note not found")
+        }
+        note.title=newTitle
+        note.text=newText
+        const updatedNote = await note.save()
+        res.status(200).json(updatedNote)
     } catch (error) {
 
-        
+        next(error)
+    }
+}
+
+// delete notes 
+export const deleteNote :RequestHandler= async(req, res, next)=>{
+    const noteId = req.params.noteId
+    try {
+        if(!mongoose.isValidObjectId(noteId)){
+            throw createHttpError(400, "invalid note id")
+        }
+        const note = await NoteModel.findById(noteId).exec()
+
+        if(!note){
+            throw createHttpError(404, "note not found")
+        }
+        await note.deleteOne()
+        res.sendStatus(204)
+           
+    } catch (error) {
+        next(error)
     }
 }
--- Backend/src/routes/notes.ts
+++ Backend/src/routes/notes.ts
@@ -1,9 +1,11 @@
 import * as NotesController from "../controllers/notes"
 import express  from "express"
 
+
 const router =express.Router()
 router.get("/",NotesController.getNotes )
 router.post("/",NotesController.createNote )
-router.get("/:noteId",NotesController.createNote )
-
+router.get("/:noteId",NotesController.getNote )
+router.patch("/:noteId", NotesController.updateNote)
+router.delete("/:noteId", NotesController.deleteNote)
 export default router
