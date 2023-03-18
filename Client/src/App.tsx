019a87213d3d6494f4ebc2511a94401594920252
sonu sharma <sonusharma224447@gmail.com>
2023-03-18 06:29:57 +0530

    feat:get all notes from server

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React ,{useEffect, useState}from "react"
 import './App.css';
+import {Note} from "./models/note"
+
 
 function App() {
+  const [notes, setNotes]=useState<Note[]>([])
+
+  useEffect(()=>{
+    const getNotes = async()=>{
+      try {
+        const response =await fetch("/api/notes")
+        const notesData = await response.json()
+        setNotes(notesData)
+        
+      } catch (error) {
+        console.log(error)
+        alert(error)
+      }
+    }
+    getNotes()
+  },[])
+
   return (
     <div className="App">
+{JSON.stringify(notes)}
+     
      
     </div>
   );
